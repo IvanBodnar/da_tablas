6762726f7ef1f62ee49d49e98270b313d4bfd0b3
observatorio <observatorio@gmail.com>
2016-06-14 13:37:10 -0300

    Agregadas busquedas

--- crear_tablas.sql
+++ crear_tablas.sql
@@ -26,8 +26,97 @@
 	franja_etaria_victima integer
 );
 
+CREATE TABLE victimas (
+	id_victima integer,
+	causa varchar(50),
+	rol varchar(50),
+	tipo varchar(150),
+	marca varchar(50),
+	modelo varchar(50),
+	colectivo varchar(25),
+	interno_colectivo varchar(25),
+	sexo varchar(25),
+	edad integer,
+	sumario integer,
+	repetido integer
+);
+
+CREATE TABLE acusados (
+	id_acusado integer,
+	rol varchar(50),
+	tipo varchar(150),
+	marca varchar(50),
+	modelo varchar(50),
+	colectivo varchar(25),
+	interno_colectivo varchar(25),
+	sexo varchar(25),
+	edad integer,
+	repetido integer
+);
+
 copy hechos from '/home/ivan/Projects/pruebas/da_tablas/hechos.csv' header csv encoding 'latin3';
 
+copy victimas from '/home/ivan/Projects/pruebas/da_tablas/victimas.csv' header csv encoding 'latin3';
+
+copy acusados from '/home/ivan/Projects/pruebas/da_tablas/acusados.csv' header csv encoding 'latin3';
+
+
+delete from acusados where repetido = 1;
+
+alter table hechos add constraint id_pk primary key (id);
+
+alter table victimas add foreign key (id_victima) references hechos(id);
+
+alter table acusados add foreign key (id_acusado) references hechos(id);
+
 select * from hechos;
+select * from victimas;
+select * from acusados;
+
+-- Join en las tres tablas
+select hechos.id, hechos.lugar_hecho, victimas.sexo, victimas.edad as edad_victima, acusados.edad as edad_acusado
+from hechos
+inner join victimas on hechos.id = id_victima
+inner join acusados on hechos.id = id_acusado; 
+
+-- Promedio de edad por mes sacando los 999 con subquery
+select periodo, avg(edad) promedio_edad
+from hechos
+join victimas
+on id = id_victima
+where edad in (select edad from victimas where edad <> 999)
+group by periodo;
+
+-- Join especificando rango de edad de las victimas
+select id, lugar_hecho, victimas.edad
+from hechos
+join victimas
+on hechos.id = id_victima
+where victimas.edad between 18 and 25
+order by id;
+
+-- Join especificando un caso concreto
+select id, victimas.sexo sexo, victimas.edad edad
+from hechos 
+join victimas
+on id = id_victima
+where id = 8893;
+
+select hechos.id, count(victimas.id_victima)
+from hechos
+join victimas
+on hechos.id = victimas.id_victima
+group by hechos.id;
+
+select id, lugar_hecho, causa
+from hechos
+right outer join victimas
+on id = id_victima
+where causa = 'HOMICIDIO'; 
+
+select id_victima, causa
+from victimas
+where causa = 'HOMICIDIO';
 
 drop table hechos;
+drop table victimas;
