ea2defaf9b7876445990f2ce74a054ed5d16a0f4
Ivan Bodnar <ivangbodnar@gmail.com>
2016-06-19 15:13:48 -0300

    Agregadas consultas pfa_2015 (api)

--- postgis.sql
+++ postgis.sql
@@ -48,3 +48,18 @@ select * from wifi
 where id = 202;
 
 
+INSERT into spatial_ref_sys (srid, auth_name, auth_srid, proj4text, srtext) values ( 97124, 'sr-org', 7124, '+proj=tmerc +lat_0=-34.6297166 +lon_0=-58.4627 +k=0.9999980000000001 +x_0=100000 +y_0=100000 +ellps=intl +units=m +no_defs ', 'PROJCS["GKBA",GEOGCS["International 1909 (Hayford)",DATUM["CAI",SPHEROID["intl",6378388,297]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",-34.6297166],PARAMETER["central_meridian",-58.4627],PARAMETER["scale_factor",0.999998],PARAMETER["false_easting",100000],PARAMETER["false_northing",100000],UNIT["Meter",1]]');
+
+
+select * from pfa_2015;
+
+select ST_GeomFromText(wkt_gk, 97124) from pfa_2015 as cosa;
+
+update pfa_2015 set geom = ST_Transform(ST_GeomFromText(wkt_gk, 97124), 4326);
+
+-- Borrar los 0 porque si no tira error ST_GeomFromText
+delete from pfa_2015 where wkt_gk = '0';
+
+update pfa_2015 set wkt_wgs = ST_AsText(geom);
+
+
